8868d75e5a8539389fb93e1844464a4ddd9225c0
QuyetTien1308 <103922226+QuyetTien1308@users.noreply.github.com>
2022-10-07 14:39:36 +0700

    Commit giup Quan, danh muc, thuong hieu

--- classes/product.php
+++ classes/product.php
@@ -19,7 +19,33 @@ class product
 		$this->db = new Database();
 		$this->fm = new Format();
 	}
-	
+	public function show_product()
+	{
+		// $query = "
+
+		// SELECT p.*,c.catName, b.brandName
+
+		// FROM tbl_product as p,tbl_category as c, tbl_brand as b where p.catId = c.catId 
+
+		// AND p.brandId = b.brandId 
+
+		// order by p.productId desc";
+
+		$query = "
+
+			SELECT tbl_product.*, tbl_category.catName, tbl_brand.brandName 
+
+			FROM tbl_product INNER JOIN tbl_category ON tbl_product.catId = tbl_category.catId 
+
+			INNER JOIN tbl_brand ON tbl_product.brandId = tbl_brand.brandId 
+
+			order by tbl_product.productId desc";
+
+		// $query = "SELECT * FROM tbl_product order by productId desc";
+
+		$result = $this->db->select($query);
+		return $result;
+	}
 	public function insert_product($data, $files)
 	{
 
