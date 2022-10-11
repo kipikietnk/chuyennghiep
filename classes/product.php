b2f5fd8f09eaa19bb7c8a620bc69453306fe3dd2
QuyetTien1308 <103922226+QuyetTien1308@users.noreply.github.com>
2022-10-11 16:21:14 +0700

    Tim kiem san pham tai trang chu

--- classes/product.php
+++ classes/product.php
@@ -199,6 +199,14 @@ class product
 	{
 		$query = "SELECT * FROM tbl_product";
 		$result = $this->db->select($query);
+		
+		return $result;
+	}
+	public function search_product($tukhoa)
+	{
+		$tukhoa = $this->fm->validation($tukhoa);
+		$query = "SELECT * FROM tbl_product WHERE productName LIKE '%$tukhoa%'";
+		$result = $this->db->select($query);
 		return $result;
 	}
 }
